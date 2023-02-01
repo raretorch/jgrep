8ae3f65a7ece7c3ab1234b73a30b404b014a02f1
raretorch <boringbuds@icloud.com>
2023-02-01 20:36:44 +0400

    Started. Added args[] preparing

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+import java.lang.*;
+
+class Main {
+
+  static String inputText;
+  static String[] searchable;
+  static String[] param;
+  static char paramCharacter = '-';
+
+  public static void main(String[] args) {
+    translateArgs(args);
+  }
+
+  static String[] translateArgs(String[] args) {
+    int paramCount = 0;
+    int argsCount = 0;
+    int i = 0;
+    // recording arrays length for initializing
+    while (i < args.length - 1) {
+      if ((args[i].toCharArray())[0] == paramCharacter) {
+        paramCount++;
+        i++;
+        continue;
+      }
+      argsCount++;
+      i++;
+    }
+    // init arrays
+    searchable = new String[argsCount];
+    param = new String[paramCount];
+
+    paramCount = 0;
+    argsCount = 0;
+    int y = 0;
+    // paste args[] to these arrays
+    while (y < args.length - 1) {
+      if ((args[y].toCharArray())[0] == paramCharacter) {
+        param[paramCount] = args[y];
+        paramCount++;
+        y++;
+        continue;
+      }
+      searchable[argsCount] = args[y];
+      argsCount++;
+      y++;
+    }
+    inputText = args[args.length - 1];
+
+    i = 0;
+    y = 0;
+    // log arrays
+    while (i < searchable.length) {
+      System.out.println("searchable[" + i + "] = " + searchable[i]);
+      i++;
+    }
+    while (y < param.length) {
+      System.out.println("parameter[" + y + "] = " + param[y]);
+      y++;
+    }
+    System.out.println("text = " + inputText);
+    return args;
+  }
+}
