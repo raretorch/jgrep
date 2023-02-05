104f3770f9d37d4836dc07c9b2904069f576a2aa
raretorch <boringbuds@icloud.com>
2023-02-05 06:46:40 +0400

    added grep

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,57 @@
 package comp.src;
 
+import java.nio.file.*;
+import java.io.*;
 import java.lang.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class Main {
 
   static String inputText;
+  static List<String> Text;
+  static ArrayList<int[]> finds = new ArrayList<>();
+  static int findsCount;
   static String[] searchable;
   static String[] param;
   static char paramCharacter = '-';
+  static boolean verbose = false;
 
   public static void main(String[] args) {
     translateArgs(args);
+    if (inputText.equalsIgnoreCase("null parameter")) {
+      return;
+    }
+    if (paramFinder("-v")) { // setting verbose mode
+      verbose = true;
+    }
+    if (verbose){setParametersLog();}
+    if (paramFinder("-f")) { //read text from file
+      if (verbose) {System.out.println("Enter file mode");}
+      try { //read the file
+        Text = Files.readAllLines(Paths.get(inputText));
+      } 
+      catch(IOException exception) {
+        System.out.println(exception.getMessage());
+      }
+    } else {
+      Text = Arrays.asList(inputText);
+    }
+    if (verbose) { //print full file text
+      System.out.println("=========================");
+      for (String str : Text) {
+        System.out.println(str);
+      }
+      System.out.println("=========================");
+    }
+    System.out.println("||||||||||||||||||||||||||||");
+    for (int f = 0; f < searchable.length; f++) {
+      int[] buffer = new int[3];
+      while ((buffer = findSequence(searchable[f], buffer[0], buffer[1]+buffer[2]))[0] < Text.size()) {
+        finds.add(buffer);
+      }
+    }
   }
 
   static String[] translateArgs(String[] args) { //setting parameters into variables
@@ -30,12 +71,15 @@ class Main {
     // init arrays
     searchable = new String[argsCount];
     param = new String[paramCount];
-
+    if (searchable.length == 0) {
+      System.out.println("jgrep | usage: jgrep [SEARCH] -PARAMETERS[-v | -f] (TEXT)");
+      inputText = "null parameter";
+      return null;
+    }
     paramCount = 0;
     argsCount = 0;
     int y = 0;
-    // paste args[] to these arrays
-    while (y < args.length - 1) {
+    while (y < args.length - 1) {  // paste args[] to these arrays
       if ((args[y].toCharArray())[0] == paramCharacter) {
         param[paramCount] = args[y];
         paramCount++;
@@ -47,9 +91,12 @@ class Main {
       y++;
     }
     inputText = args[args.length - 1];
+    return args;
+  }
 
-    i = 0;
-    y = 0;
+  static void setParametersLog() { //logging parameters
+    int i = 0;
+    int y = 0;
     // log arrays
     while (i < searchable.length) {
       System.out.println("searchable[" + i + "] = " + searchable[i]);
@@ -60,6 +107,37 @@ class Main {
       y++;
     }
     System.out.println("text = " + inputText);
-    return args;
+    return;
+  }
+
+  static boolean paramFinder(String parameter) { //find parameter in param[]
+    if (param.length == 0) {
+      return false;
+    }
+    for (int i = 0; i < param.length; i++) {
+      if(param[i].equalsIgnoreCase(parameter)) {
+        if (verbose) {System.out.println("Find " + parameter + " parameter");}
+        return true;
+      } 
+    }
+    return false;
+  }
+
+  static int[] findSequence (String search, int start, int indexFrom) { //find one word in a line
+    int[] index = new int[3];
+    int ch;
+    if ((ch = Text.get(start).indexOf(search, indexFrom)) != -1) {
+      index[1] = ch;
+      index[0] = start;
+      index[2] = search.length();
+      if (verbose) {System.out.println("String: " + index[0] + "; Index: " + index[1] + "; Length:" + index[2]);}
+      return index;
+    } 
+    else {
+      index[0] = start +1;
+      index[1] = 0;
+      index[2] = 0;
+      return index;
+    }
   }
 }
